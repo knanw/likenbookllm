69826c65e5ebf16bd44b9b14269a36acb01f6998
knanw <knanw@outlook.com>
2026-06-22 22:19:39 +0200

    cors bugfix

--- backend/main.py
+++ backend/main.py
@@ -21,11 +21,8 @@ app = FastAPI(title="Notebook Lite API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    #allow_origins=["[localhost](http://localhost:3000)"],
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
